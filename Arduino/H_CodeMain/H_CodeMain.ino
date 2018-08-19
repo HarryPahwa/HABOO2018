fe3a52b802b86296dad3948319268222a309a052
Harpreet Singh <harrypahwa95@gmail.com>
2018-08-19 17:55:22 -0400

    Init

--- Arduino/H_CodeMain/H_CodeMain.ino
+++ Arduino/H_CodeMain/H_CodeMain.ino
@@ -4,10 +4,10 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
-#define BME_SCK 13
-#define BME_MISO 12
-#define BME_MOSI 11
-#define BME_CS 10
+//#define BME_SCK 13
+//#define BME_MISO 12
+//#define BME_MOSI 11
+//#define BME_CS 10
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
